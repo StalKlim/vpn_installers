9ff451288163fe516069e66d790fef76a4b90e11
StalKlim <klimovvals@mail.ru>
2023-08-08 23:10:47 +0400

    Updated download links

--- original_files/update.sh
+++ original_files/update.sh
@@ -7,7 +7,7 @@ apt install ca-certificates --yes
 timedatectl set-timezone Europe/Samara
 rm openvpn-install.sh
 rm wireguard-install.sh
-wget -O wire.sh https://goo.su/wrzCY # https://raw.githubusercontent.com/Nyr/wireguard-install/master/wireguard-install.sh
-wget -O open.sh https://goo.su/ELYYu # https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
-wget -O shadow.sh https://goo.su/naBcRd # https://raw.githubusercontent.com/unixhostpro/shadowsocks-simple-install/master/shadowsocks-simple-install.sh
-wget -O shadowR.sh https://goo.su/kKx5 # https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+wget -O wire.sh https://goo.su/TBEZ2yd
+wget -O open.sh https://goo.su/UknA1z
+wget -O shadow.sh https://goo.su/nIuNX
+wget -O shadowR.sh https://goo.su/Pz0Qp
