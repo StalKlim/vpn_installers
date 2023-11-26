6431f30b99b389c94e698655265df00d850311a8
StalKlim <klimovvals@mail.ru>
2023-11-27 03:51:32 +0400

    Add files via upload

--- original_files/update.sh
+++ original_files/update.sh
@@ -5,11 +5,11 @@ apt install locales-all
 update-locale LANG=ru_RU.UTF-8 
 apt install ca-certificates --yes 
 apt install curl --yes
+apt install python3 --yes
 timedatectl set-timezone Europe/Samara
 rm openvpn-install.sh
 rm wireguard-install.sh
 mkdir confvl
-apt install python3 --yes
 
 wget -O update.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/update.sh
 wget -O wire.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/wire.sh
