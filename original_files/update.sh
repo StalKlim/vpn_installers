62cca1765b3c46523a0c0a8330da1d49d7efcceb
StalKlim <klimovvals@mail.ru>
2023-11-27 03:20:34 +0400

    Add files via upload

--- original_files/update.sh
+++ original_files/update.sh
@@ -4,10 +4,12 @@ apt-get update && apt-get upgrade --yes
 apt install locales-all 
 update-locale LANG=ru_RU.UTF-8 
 apt install ca-certificates --yes 
+apt install curl --yes
 timedatectl set-timezone Europe/Samara
 rm openvpn-install.sh
 rm wireguard-install.sh
-mkdir vless
+mkdir confvl
+apt install python3 --yes
 
 wget -O update.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/update.sh
 wget -O wire.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/wire.sh
@@ -15,6 +17,6 @@ wget -O open.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/o
 wget -O shadow.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/shadow.sh
 wget -O shadowR.sh https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/shadowR.sh
 wget -O vless.py https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/vless.py
-wget -O vless/configgrpc.json https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/configgrpc.json
-wget -O vless/configh2.json https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/configh2.json
-wget -O vless/configxtls.json https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/configxtls.json
+wget -O confvl/configgrpc.json https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/configgrpc.json
+wget -O confvl/configh2.json https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/configh2.json
+wget -O confvl/configxtls.json https://raw.githubusercontent.com/StalKlim/vpn_installers/main/original_files/configxtls.json
